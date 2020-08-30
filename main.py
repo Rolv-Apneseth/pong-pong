7f83c6384937aef8cb196d029285035692e3194c
Rolv-Apneseth <rolv.apneseth@gmail.com>
2020-08-30 23:56:38 +0100

    Initial upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import pygame
+
+import pong_pong
+
+def main_menu(window, WIDTH, HEIGHT):
+    title_font = pygame.font.SysFont("arial", 40)
+
+    run = True
+    while run:
+        #Background
+        window.fill(pong_pong.BLACK)
+
+        #Labels
+        title_label = title_font.render("Welcome to Pong Pong!", 1, pong_pong.WHITE)
+        title_middle = WIDTH//2 - title_label.get_width()//2
+        window.blit(title_label, (title_middle, HEIGHT//5))
+
+        click_label = title_font.render("To begin the game, simply left click.", 1, pong_pong.WHITE)
+        click_middle = WIDTH//2 - click_label.get_width()//2
+        window.blit(click_label, (click_middle, HEIGHT*2//5 ))
+
+        controls_label1 = title_font.render("Player 1 moves with A and D", 1, pong_pong.WHITE)
+        control1_middle = WIDTH//2 - controls_label1.get_width()//2
+        window.blit(controls_label1, (control1_middle, HEIGHT*6//10))
+
+        controls_label2 = title_font.render("Player 2 moves with left and right", 1, pong_pong.WHITE)
+        control2_middle = WIDTH//2 - controls_label2.get_width()//2
+        window.blit(controls_label2, (control1_middle, HEIGHT*7//10))
+
+
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+
+
+
+def main():
+    pygame.font.init()
+
+    WIDTH, HEIGHT = 750, 750
+    WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    pygame.display.set_caption("Pong Pong")
+
+    main_menu(WIN, WIDTH, HEIGHT)
+    pong_pong.game_loop(WIN, WIDTH, HEIGHT)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ pong_pong.py
@@ -0,0 +1,174 @@
+import pygame
+import math
+import random
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (147,112,219)
+
+class Paddle():
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+        self.surface = pygame.Surface((self.width, self.height))
+        self.surface.fill(WHITE)
+
+        self.points = 0
+
+
+    def draw(self, window):
+        window.blit(self.surface, (self.x, self.y))
+
+
+    def move(self, vel):
+        self.x += vel
+
+
+
+class Goal():
+    def __init__(self, y, width, height):
+        self.x = 0
+        self.y = y
+        self.width = width
+        self.height = height
+
+        self.surface = pygame.Surface((self.width, self.height))
+        self.surface.fill(PURPLE)
+
+
+    def draw(self, window):
+        window.blit(self.surface, (self.x, self.y))
+
+
+
+class Ball():
+    COOLDOWN = 30 #0.5 seconds at 60 fps
+
+    def __init__(self, x, y, vel, radius):
+        self.x = x
+        self.y = y
+        self.xvel = vel
+        self.yvel = vel
+        self.cooldown_counter = self.COOLDOWN
+        self.radius = radius
+
+
+    def reset_cooldown(self):
+        """Cooldown so that ball does not start moving striaght away"""
+        self.cooldown_counter = self.COOLDOWN
+
+
+    def move(self):
+        """Moves ball if cooldown is 0"""
+        if self.cooldown_counter:
+            self.cooldown_counter -= 1
+        else:
+            self.x += self.xvel
+            self.y += self.yvel
+
+
+    def draw(self, window):
+        pygame.draw.circle(window, WHITE, (int(self.x), int(self.y)), self.radius)
+
+
+
+def game_loop(window, WIDTH, HEIGHT):
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    BALL_RADIUS = 15
+    BALL_VELOCITY = 3
+    #Starting location for the ball, and place where it respawns
+    DEFAULT_X = WIDTH//2
+    DEFAULT_Y = HEIGHT//2
+
+    PADDLE_VELOCITY = 5
+
+    running = True
+
+    #Goals, point scored if ball hits enemy goal object
+    top_goal = Goal(round(HEIGHT*0.1), WIDTH, round(HEIGHT*0.02))
+    bottom_goal = Goal(round(HEIGHT*0.96), WIDTH, round(HEIGHT*0.02))
+
+    player1 = Paddle(round(WIDTH*0.05), round(HEIGHT*0.13), round(WIDTH*0.1), round(HEIGHT*0.02))
+    player2 = Paddle(round(WIDTH*0.85), round(HEIGHT*0.93), round(WIDTH*0.1), round(HEIGHT*0.02))
+
+    ball = Ball(DEFAULT_X, DEFAULT_Y, BALL_VELOCITY, BALL_RADIUS)
+
+    score_label_font = pygame.font.SysFont("arial", 20)
+
+    def redraw_window(window):
+        """Updates ui, executed once every frame."""
+
+        #Background
+        window.fill(BLACK)
+
+        #Display Objects. Have to be redrawn because objects are moving every frame.
+        top_goal.draw(window)
+        bottom_goal.draw(window)
+
+        player1.draw(window)
+        player2.draw(window)
+
+        ball.draw(window)
+
+        #Player Score Labels
+        player1_score = score_label_font.render(f"Player 1 score: {player1.points}", 1, WHITE)
+        window.blit(player1_score, (WIDTH//10, HEIGHT//15))
+
+        player2_score = score_label_font.render(f"Player 2 score: {player2.points}", 1, WHITE)
+        window.blit(player2_score, (WIDTH*7//10, HEIGHT//15))
+
+
+        pygame.display.update()
+
+    while running:
+        clock.tick(FPS)
+
+        redraw_window(window)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        #MOVEMENT
+        keys = pygame.key.get_pressed()
+
+        #player1, uses a and d keys
+        if keys[pygame.K_a] and player1.x - PADDLE_VELOCITY > 0: #left
+            player1.move(-PADDLE_VELOCITY)
+        if keys[pygame.K_d] and player1.x + PADDLE_VELOCITY + player1.width < WIDTH: #right
+            player1.move(PADDLE_VELOCITY)
+        #player2, uses up and down arrow keys
+        if keys[pygame.K_LEFT] and player2.x - PADDLE_VELOCITY > 0: #left
+            player2.move(-PADDLE_VELOCITY)
+        if keys[pygame.K_RIGHT] and player2.x + PADDLE_VELOCITY + player2.width < WIDTH: #right
+            player2.move(PADDLE_VELOCITY)
+
+        #Ball movement and collisions
+        ball.move()
+        #Contain in the x boundaries of the screen
+        if ball.x < ball.radius or ball.x > WIDTH - ball.radius:
+            ball.xvel *= -1
+        #Collision with paddles, causes ball velocity to reverse
+        if ball.y - ball.radius < player1.y + player1.height and player1.x < ball.x < player1.x + player1.width:
+            ball.yvel *= -1
+        if ball.y + ball.radius > player2.y and player2.x < ball.x < player2.x + player2.width:
+            ball.yvel *= -1
+        #Collision with goals, point added to respective player
+        if ball.y + ball.radius > bottom_goal.y:
+            ball.reset_cooldown()
+            ball.x, ball.y = DEFAULT_X, DEFAULT_Y
+            ball.xvel = random.choice([1, -1]) * BALL_VELOCITY
+            ball.yvel = random.choice([1, -1]) * BALL_VELOCITY
+            player1.points += 1
+        #Two different if statements so that point can be awarded to the correct player
+        if ball.y - ball.radius < top_goal.y + top_goal.height:
+            ball.reset_cooldown()
+            ball.x, ball.y = DEFAULT_X, DEFAULT_Y
+            ball.xvel = random.choice([1, -1]) * BALL_VELOCITY
+            ball.yvel = random.choice([1, -1]) * BALL_VELOCITY
+            player2.points += 1
