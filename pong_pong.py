cbd263a4f74f5365fbbc62258c58646c78d00d68
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-10-29 19:35:14 +0000

    Reformatted using black and isort

--- pong_pong.py
+++ pong_pong.py
@@ -1,12 +1,13 @@
-import pygame
 import random
+
+import pygame
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 PURPLE = (147, 112, 219)
 
 
-class Paddle():
+class Paddle:
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
@@ -25,7 +26,7 @@ class Paddle():
         self.x += vel
 
 
-class Goal():
+class Goal:
     def __init__(self, y, width, height):
         self.x = 0
         self.y = y
@@ -39,7 +40,7 @@ class Goal():
         window.blit(self.surface, (self.x, self.y))
 
 
-class Ball():
+class Ball:
     # So ball waits before mobing in a direction
     COOLDOWN = 30  # 0.5 seconds at 60 fps
 
@@ -65,8 +66,7 @@ class Ball():
             self.y += self.yvel
 
     def draw(self, window):
-        pygame.draw.circle(
-            window, WHITE, (int(self.x), int(self.y)), self.radius)
+        pygame.draw.circle(window, WHITE, (int(self.x), int(self.y)), self.radius)
 
 
 def game_loop(window, WIDTH, HEIGHT):
@@ -94,10 +94,18 @@ def game_loop(window, WIDTH, HEIGHT):
     top_goal = Goal(round(HEIGHT * 0.1), WIDTH, round(HEIGHT * 0.02))
     bottom_goal = Goal(round(HEIGHT * 0.96), WIDTH, round(HEIGHT * 0.02))
 
-    player1 = Paddle(round(WIDTH * 0.05), round(HEIGHT * 0.13),
-                     round(WIDTH * 0.1), round(HEIGHT * 0.02))
-    player2 = Paddle(round(WIDTH * 0.85), round(HEIGHT * 0.93),
-                     round(WIDTH * 0.1), round(HEIGHT * 0.02))
+    player1 = Paddle(
+        round(WIDTH * 0.05),
+        round(HEIGHT * 0.13),
+        round(WIDTH * 0.1),
+        round(HEIGHT * 0.02),
+    )
+    player2 = Paddle(
+        round(WIDTH * 0.85),
+        round(HEIGHT * 0.93),
+        round(WIDTH * 0.1),
+        round(HEIGHT * 0.02),
+    )
 
     balls = []
     balls.append(Ball(DEFAULT_X, DEFAULT_Y, BALL_VELOCITY, BALL_RADIUS))
@@ -123,23 +131,24 @@ def game_loop(window, WIDTH, HEIGHT):
 
         # Player Score Labels
         player1_score = score_label_font.render(
-            f"Player 1 score: {player1.points}", 1, WHITE)
+            f"Player 1 score: {player1.points}", 1, WHITE
+        )
         window.blit(player1_score, (WIDTH // 10, HEIGHT // 15))
 
         player2_score = score_label_font.render(
-            f"Player 2 score: {player2.points}", 1, WHITE)
+            f"Player 2 score: {player2.points}", 1, WHITE
+        )
         window.blit(player2_score, (WIDTH * 7 // 10, HEIGHT // 15))
 
         # Displays win label
         if win:
-            win_label = win_label_font.render(f"{who_won} won the game!",
-                                              1,
-                                              (255, 255, 255)
-                                              )
+            win_label = win_label_font.render(
+                f"{who_won} won the game!", 1, (255, 255, 255)
+            )
 
-            window.blit(win_label,
-                        (WIDTH // 2 - win_label.get_width() // 2, HEIGHT // 2)
-                        )
+            window.blit(
+                win_label, (WIDTH // 2 - win_label.get_width() // 2, HEIGHT // 2)
+            )
 
         pygame.display.update()
 
@@ -176,18 +185,21 @@ def game_loop(window, WIDTH, HEIGHT):
         # player1, uses a and d keys
         if keys[pygame.K_a] and player1.x - PADDLE_VELOCITY > 0:  # left
             player1.move(-PADDLE_VELOCITY)
-        if keys[pygame.K_d] and player1.x + PADDLE_VELOCITY + player1.width < WIDTH:  # right
+        if (
+            keys[pygame.K_d] and player1.x + PADDLE_VELOCITY + player1.width < WIDTH
+        ):  # right
             player1.move(PADDLE_VELOCITY)
         # player2, uses up and down arrow keys
         if keys[pygame.K_LEFT] and player2.x - PADDLE_VELOCITY > 0:  # left
             player2.move(-PADDLE_VELOCITY)
-        if keys[pygame.K_RIGHT] and player2.x + PADDLE_VELOCITY + player2.width < WIDTH:  # right
+        if (
+            keys[pygame.K_RIGHT] and player2.x + PADDLE_VELOCITY + player2.width < WIDTH
+        ):  # right
             player2.move(PADDLE_VELOCITY)
         # Either player can press spacbar to create another ball
         if keys[pygame.K_SPACE]:
             if ball_spawn_cooldown >= 120:
-                balls.append(Ball(DEFAULT_X, DEFAULT_Y,
-                                  BALL_VELOCITY, BALL_RADIUS))
+                balls.append(Ball(DEFAULT_X, DEFAULT_Y, BALL_VELOCITY, BALL_RADIUS))
                 ball_spawn_cooldown = 0
 
         # Ball movement and collisions
@@ -197,9 +209,15 @@ def game_loop(window, WIDTH, HEIGHT):
             if ball.x < ball.radius or ball.x > WIDTH - ball.radius:
                 ball.xvel *= -1
             # Collision with paddles, causes ball velocity to reverse
-            if ball.y - ball.radius < player1.y + player1.height and player1.x < ball.x < player1.x + player1.width:
+            if (
+                ball.y - ball.radius < player1.y + player1.height
+                and player1.x < ball.x < player1.x + player1.width
+            ):
                 ball.yvel *= -1
-            if ball.y + ball.radius > player2.y and player2.x < ball.x < player2.x + player2.width:
+            if (
+                ball.y + ball.radius > player2.y
+                and player2.x < ball.x < player2.x + player2.width
+            ):
                 ball.yvel *= -1
             # Collision with goals, point added to respective player
             if ball.y + ball.radius > bottom_goal.y:
